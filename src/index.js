13dfed6fa192036822a25f54407d00d27cc623de
bo wolters <bowolters@Bos-M1-Max.local>
2021-12-07 11:41:14 +0100

    save

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ $(document).ready(function () {
 	const displayImages = function (res) {
 		const imgArr = res.results;
 
-		//$('.w-imgr_image_container').css('display', 'flex');
 		$('.w-imgr_image_container').css('display', 'inline-grid');
 
 		// index variable for updating index served into imgArr
@@ -42,7 +41,6 @@ $(document).ready(function () {
 		imgArr.forEach(() => {
 			index++;
 			idIndex++;
-			const imgSrcThumb = imgArr[index].urls.thumb;
 			const imgSrcSmall = imgArr[index].urls.small;
 			const imgSrcFull = imgArr[index].urls.full;
 
@@ -50,8 +48,7 @@ $(document).ready(function () {
         <div class="w-imgr_image_wrapper">
         <div class="w-imgr_unsplash_image" id="w-imgr_unsplash-img-${idIndex}">
 		<div class="w-imgr_unsplash_image_cover"></div>
-          <div class="w-imgr_select_btn" id="w-imgr_select_btn-${idIndex}" ><img class="w-imgr_select_icon" src="${checkIcon}"><h5 class="w-imgr_select_btn_text">Selecteer afbeelding</h5></div>
-                <div class="w-imgr_attribution_wrapper">
+                <div class="w-imgr_attribution_wrapper" id="w=imgr_attribution_wrapper-${idIndex}">
                   <h5 class="w-imgr_creator_name">Photo by:  
                     <a class="w-imgr_attribution_link" target="_blank" href="${imgArr[index].user.links.html}?utm_source=w-imgr&utm_medium=referral" >${imgArr[index].user.name}</a>
                   </h5>
@@ -70,92 +67,61 @@ $(document).ready(function () {
 			);
 			$(`#w-imgr_unsplash-img-${idIndex}`).attr('data', `${imgSrcFull}`);
 		});
+
 		// when image is selected load in hd quality image from data attr as background-image
-		$('.w-imgr_image_wrapper').mouseenter(function () {
-			$(this)
-				.children('.w-imgr_unsplash_image')
-				.children('.w-imgr_select_btn')
-				.css('opacity', '1');
-			$(this)
-				.children('.w-imgr_unsplash_image')
-				.children('.w-imgr_select_btn')
-				.click(function () {
-					// change Close button styling while image is selected
-					$('.w-imgr_close_modal_btn_wrapper').addClass(
-						'w-imgr_close_modal_btn_animation'
-					);
+		$('.w-imgr_image_wrapper').click(function () {
+			$('.w-imgr_close_modal_btn_wrapper').addClass(
+				'w-imgr_close_modal_btn_animation'
+			);
+			const imageLink = $(this).children('.w-imgr_unsplash_image').attr('data');
+			$('#w-imgr_form_input').val(imageLink);
 
-					$('.w-imgr_select_icon').removeClass('w-imgr_select_icon_show');
-					$(this).children('img').toggleClass('w-imgr_select_icon_show');
-					$('.w-imgr_select_btn').attr('id', '');
-					$(this).attr('id', 'w-imgr_selected_btn');
-					$('.w-imgr_select_btn').css('opacity', '0');
-					$('.w-imgr_select_btn_text').html('Selecteer afbeelding');
-					$(this)
-						.children('.w-imgr_select_btn_text')
-						.html('Afbeelding gekozen');
-					$(this).css('opacity', '1');
-					const imageLink = $(this)
-						.parent('.w-imgr_unsplash_image')
-						.attr('data');
-					$('#w-imgr_form_input').val(imageLink);
+			$('.w-imgr_image_wrapper_selected').removeClass(
+				'w-imgr_image_wrapper_selected'
+			);
 
-					$('.w-imgr_image_wrapper_selected').removeClass(
-						'w-imgr_image_wrapper_selected'
-					);
-					$(this).parent().addClass('w-imgr_image_wrapper_selected');
-					(async () => {
-						const img = new Image();
-						img.src = `${$(this)
-							.parent('.w-imgr_unsplash_image')
-							.attr('data')}`;
+			// WHEN IMAGED SELECTED APPLY STUFF HERE
+			$(this).addClass('w-imgr_image_wrapper_selected');
 
-						$(`#w-imgr_preview_image`).append(
-							$('<div>')
-								.addClass('w-imgr_loading_animation')
-								.css('background-image', `url(${loadIcon})`)
-						);
-						$(`#w-imgr_preview_image`).append(
-							$('<h5>Loading</h5>').addClass('w-imgr_loading_text')
-						);
-						await img.decode();
-						$('.w-imgr_loading_animation').remove();
-						$('.w-imgr_loading_text').remove();
-						$(`#w-imgr_preview_image`).css(
-							'background-image',
-							`url(${$(this).parent('.w-imgr_unsplash_image').attr('data')}`
-						);
-					})();
-					$('#w-imgr_remove_btn').css('display', 'block');
-				});
-		});
+			(async () => {
+				const img = new Image();
+				img.src = `${imageLink}`;
 
-		$('.w-imgr_image_wrapper').mouseleave(function () {
-			if (
-				$(this)
-					.children('.w-imgr_unsplash_image')
-					.children('.w-imgr_select_btn')
-					.attr('id') !== 'w-imgr_selected_btn'
-			) {
-				$(this)
-					.children('.w-imgr_unsplash_image')
-					.children('.w-imgr_select_btn')
-					.css('opacity', '0');
-			} else {
-				$(this)
-					.children('.w-imgr_unsplash_image')
-					.children('.w-imgr_select_btn')
-					.css('opacity', '1');
-			}
+				$(`#w-imgr_preview_image`).append(
+					$('<div>')
+						.addClass('w-imgr_loading_animation')
+						.css('background-image', `url(${loadIcon})`)
+				);
+				$(`#w-imgr_preview_image`).append(
+					$('<h5>Loading</h5>').addClass('w-imgr_loading_text')
+				);
+				await img.decode();
+				$('.w-imgr_loading_animation').remove();
+				$('.w-imgr_loading_text').remove();
+				$(`#w-imgr_preview_image`).css('background-image', `url(${imageLink}`);
+			})();
+			$('#w-imgr_remove_btn').css('display', 'block');
 		});
 
 		index = -1;
+	};
+
+	const displayLoading = function () {
+		const html = `<div class="w-imgr_api_loader"></div>`;
+		$('.w-imgr_modal_wrapper').append(html);
+	};
+
+	const hideLoading = function () {
+		$('.w-imgr_api_loader').remove();
 	};
 
 	let pageNumber = 1;
 	// Get images on search query
 	const loadImages = async function (query) {
 		console.log('pagenumber', pageNumber);
+
+		displayLoading();
+
 		let getImages = await fetch(
 			`${proxy}https://api.unsplash.com/search/photos?&orientation=landscape&page=${pageNumber}&per_page=30&query=${query}>`,
 			{
@@ -166,6 +132,8 @@ $(document).ready(function () {
 
 		let res = await getImages.json();
 		console.log('response', res);
+
+		hideLoading();
 
 		displayImages(res);
 		console.log('images amount: ', $('.w-imgr_unsplash_image').length);
